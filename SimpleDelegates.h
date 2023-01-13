0ea8bae9f622d8f53bc3b9d7550833cc5eca697c
Franciszek Łuczak <56699656+kenarf02@users.noreply.github.com>
2023-01-13 18:55:41 +0100

    Error fixes + added readme
    consider it as an initial commit

--- SimpleDelegates.h
+++ SimpleDelegates.h
@@ -14,61 +14,64 @@
 //uncomment to remove asserts
 //#define NDEBUG
 
-template <typename>
-class Delegate;
-
-template <typename ReturnType, typename... Args>
-class Delegate<ReturnType(Args...)>
+namespace sdel
 {
-public:
-	/**
-	 * Binds a member  function \param a_method  of type:
-	 * \tparam T of an instance: \param t 
-	 */
-	template<typename T>
-	void bind(T* t, ReturnType(T::* a_method)(Args...))
-	{
-		std::function<ReturnType(Args...)> tempFunction = [=](Args ... as) { (t->*a_method)(as...); };
-		m_functions.insert(std::pair<std::pair<void*, void*>, std::function<ReturnType(Args...)>>(std::pair<void*, void*>(static_cast<void*>(t), (void*)(&a_method)), tempFunction));
-	}
+	template <typename>
+	class Delegate;
 
-	/**
-	 * \Unbinds member function \param a_method from instance \param t  of type \tparam T 
-	 */
-	template<typename T>
-	void unbind(T* t, ReturnType(T::* a_method)(Args...))
+	template <typename ReturnType, typename... Args>
+	class Delegate<ReturnType(Args...)>
 	{
-		auto found = m_functions.find(std::pair<void*, void*>(static_cast<void*>(t), static_cast<void*>(&a_method)));
-		assert(found == m_functions.end() , "Trying to unbind function that is not bound");
-		m_functions.erase(found);
-	}
-
-	/**
-	 * Invoke all bound functions. Can call with arguments
-	 * of previously defined types.IMPORTANT: It is not recommended
-	 * to bind or call anything with type other than void- it is possible,
-	 * but the return value will be discarded...
-	 */
-	void operator() (Args... args)
-	{
-		std::map<std::pair<void*, void*>, std::function<ReturnType(Args...)>> duplicate_functions = m_functions;
-		if (m_functions.size() == 0)return;
-		for (auto& func : duplicate_functions)
+	public:
+		/**
+		 * Binds a member  function \param a_method  of type:
+		 * \tparam T of an instance: \param t
+		 */
+		template<typename T>
+		void bind(T* t, ReturnType(T::* a_method)(Args...))
 		{
-			func.second(args...);
+			std::function<ReturnType(Args...)> tempFunction = [=](Args ... as) { (t->*a_method)(as...); };
+			m_functions.insert(std::pair<std::pair<void*, void*>, std::function<ReturnType(Args...)>>(std::pair<void*, void*>(static_cast<void*>(t), (void*)(&a_method)), tempFunction));
 		}
-	}
 
-	/**
-	 * \this removes all bound functions at once.
-	 */
-	void clear()
-	{
-		m_functions.clear();
-	}
+		/**
+		 * \Unbinds member function \param a_method from instance \param t  of type \tparam T
+		 */
+		template<typename T>
+		void unbind(T* t, ReturnType(T::* a_method)(Args...))
+		{
+			auto found = m_functions.find(std::pair<void*, void*>(static_cast<void*>(t), static_cast<void*>(&a_method)));
+			assert(found == m_functions.end(), "Trying to unbind function that is not bound");
+			m_functions.erase(found);
+		}
 
-private: 
-	std::unordered_map<std::pair<void*,void*>, std::function<ReturnType(Args...)>> m_functions = {};
-};
+		/**
+		 * Invoke all bound functions. Can call with arguments
+		 * of previously defined types.IMPORTANT: It is not recommended
+		 * to bind or call anything with type other than void- it is possible,
+		 * but the return value will be discarded...
+		 */
+		void operator() (Args... args)
+		{
+			std::unordered_map<std::pair<void*, void*>, std::function<ReturnType(Args...)>> duplicate_functions = m_functions;
+			if (m_functions.size() == 0)return;
+			for (auto& func : duplicate_functions)
+			{
+				func.second(args...);
+			}
+		}
 
+		/**
+		 * \this removes all bound functions at once.
+		 */
+		void clear()
+		{
+			m_functions.clear();
+		}
+
+	private:
+		std::unordered_map<std::pair<void*, void*>, std::function<ReturnType(Args...)>> m_functions = {};
+	};
+
+}
 
