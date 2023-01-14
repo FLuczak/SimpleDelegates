dbca52ae988558cf88f0309d616aadcc7c8ee0f3
Franciszek Łuczak <56699656+kenarf02@users.noreply.github.com>
2023-01-14 12:36:32 +0100

    Extended void cast to accept multiple arguments  apart from none arguments

--- SimpleDelegates.h
+++ SimpleDelegates.h
@@ -28,7 +28,7 @@ namespace sdel
 		 * \tparam T of an instance: \param t
 		 */
 		template<typename T>
-		void bind(T* t, ReturnType(T::* a_method)(Args...))
+		void Bind(T* t, ReturnType(T::* a_method)(Args...))
 		{
 			std::function<ReturnType(Args...)> tempFunction = [=](Args ... as) { (t->*a_method)(as...); };
 			auto p = void_cast(a_method);
@@ -39,7 +39,7 @@ namespace sdel
 		 * Binds a global  function \param a_method  of type:
 		 * \tparam T
 		*/
-		void bind(ReturnType(*a_method)(Args...))
+		void Bind(ReturnType(*a_method)(Args...))
 		{
 			std::function<ReturnType(Args...)> tempFunction = [=](Args ... as) { (*a_method)(as...); };
 			m_functions.insert(std::pair<std::pair<void*, int*>, std::function<ReturnType(Args...)>>(std::pair<void*, int*>(static_cast<void*>(this), (int*)a_method), tempFunction));
@@ -49,10 +49,10 @@ namespace sdel
 		 * \Unbinds member function \param a_method from instance \param t  of type \tparam T
 		 */
 		template<typename T>
-		void unbind(T* t, ReturnType(T::* a_method)(Args...))
+		void Unbind(T* t, ReturnType(T::* a_method)(Args...))
 		{
 			auto found = m_functions.find(std::pair<void*, int*>(static_cast<void*>(t), ((int*)void_cast(a_method))));
-			assert(found != m_functions.end());//ARE YOU TRYING TO BIND A NOT BOUND FUNCTION? IT IS FORBIDDEN!!
+			assert(found != m_functions.end());
 			m_functions.erase(found);
 		}
 
@@ -61,10 +61,10 @@ namespace sdel
 		 * \Unbinds global function \param a_method
 		 */
 		template<typename T>
-		void unbind(ReturnType(T::* a_method)(Args...))
+		void Unbind(ReturnType(T::* a_method)(Args...))
 		{
 			auto found = m_functions.find(std::pair<void*, void(*)()>(static_cast<void*>(this), static_cast<void(*)()>(a_method)));
-			assert(found != m_functions.end());//ARE YOU TRYING TO BIND A NOT BOUND FUNCTION? IT IS FORBIDDEN!!
+			assert(found != m_functions.end());
 			m_functions.erase(found);
 		}
 
@@ -87,7 +87,7 @@ namespace sdel
 		/*
 		 * \this removes all bound functions at once.
 		 */
-		void clear()
+		void Clear()
 		{
 			m_functions.clear();
 		}
@@ -97,13 +97,12 @@ namespace sdel
 
 
 		//void cast is an idea taken from:https://stackoverflow.com/a/37502759/20211112 thanks to Karlo Mlicevic
-		//Cannot tell if is stable and to what extent, but according to tests it works 
 		template<typename T, typename R>
-		void* void_cast(R(T::* f)())
+		void* void_cast(R(T::* f)(Args...))
 		{
 			union
 			{
-				R(T::* pf)();
+				R(T::* pf)(Args...);
 				void* p;
 			};
 			pf = f;
